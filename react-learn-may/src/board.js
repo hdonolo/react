298e579897dde950db569e55355e6cdbf17ddb94
hdonolo <87991112+hdonolo@users.noreply.github.com>
2024-05-06 15:50:54 -0400

    Changed button to span where the move is shown

--- react-learn-may/src/board.js
+++ react-learn-may/src/board.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
-function Square({value, onSquareClick}) {
-  return <button 
+function Square({ value, onSquareClick }) {
+  return <button
     className="square"
     onClick={onSquareClick}
   >{value}</button>;
@@ -30,12 +30,23 @@ export default function Game() {
     } else {
       description = 'Go to game start';
     }
-    return (
-      <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
-      </li>
-    );
+    if (move === currentMove && currentMove !== 0) {
+      description = 'You are in move #' + move;
+      return (
+        <li key={move}>
+          <span>{description}</span>
+        </li>
+      );
+    } else {
+      return (
+        <li key={move}>
+          <button onClick={() => jumpTo(move)}>{description}</button>
+        </li>
+      );
+    }
+
   });
+  console.log(moves)
   return (
     <div className="game">
       <div className="game-board">
@@ -69,27 +80,27 @@ export function Board({ xIsNext, squares, onPlay }) {
     status = 'Next player: ' + (xIsNext ? 'X' : 'O');
   }
 
-    return (
-        <>
-        <div className="status">{status}</div>
-          <div className="board-row">
-            <Square value={squares[0]}  onSquareClick={() => handleClick(0)} />
-            <Square value={squares[1]}  onSquareClick={() => handleClick(1)}  />
-            <Square value={squares[2]}  onSquareClick={() => handleClick(2)}  />
-          </div>
-          <div className="board-row">
-            <Square value={squares[3]}  onSquareClick={() => handleClick(3)}  />
-            <Square value={squares[4]}  onSquareClick={() => handleClick(4)}  />
-            <Square value={squares[5]}  onSquareClick={() => handleClick(5)}  />
-          </div>
-          <div className="board-row">
-            <Square value={squares[6]}  onSquareClick={() => handleClick(6)}  />
-            <Square value={squares[7]}  onSquareClick={() => handleClick(7)}  />
-            <Square value={squares[8]}  onSquareClick={() => handleClick(8)}  />
-          </div>
-        </>
-      );
-    
+  return (
+    <>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
+  );
+
 }
 
 function calculateWinner(squares) {
